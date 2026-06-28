7daa42f5a566b8eb3b78f6ef8c9a654bc6ffe093
githubrsjmlgagn <96869234+githubrsjmlgagn@users.noreply.github.com>
2026-06-29 05:31:55 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // PENTING: naikkan angka versi ini SETIAP kali ada file yang diedit,
 // jika tidak, HP staf akan tetap memuat versi lama dari cache.
-const CACHE = 'cda-penjualan-v21.3';
+const CACHE = 'cda-penjualan-v21.4';
 const ASSETS = [
   './',
   './index.html',
